d2b9202102eb7abb0e89ca0fdfb2a2aedbb05dab
Tomáš Konrády <tomaskonrady@gmail.com>
2018-12-12 01:04:26 +0100

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-import App from './components/App';
+// import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import { createStore } from 'redux';
+
+const reducer = (state = 0, action) => {
+	if (action.type === 'INCREMENT') {
+		return state + 1;
+	}
+	return state;
+};
+
+const store = createStore(reducer);
+store.subscribe(() => {
+	console.log('New state');
+
+	console.log(store.getState());
+});
+
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+store.dispatch({ type: 'INCREMENT' });
+
+console.log(store);
